e6d412fb7a3cbc39899cb1a646ec90a71b00fd30
rrzadca <rrzadca@gmail.com>
2023-11-02 06:40:34 +0100

    add typeorm and mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import {UsersModule} from "./crud/users/users.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: '127.0.0.1',
+        port: 3306,
+        username: 'root',
+        password: 'example',
+        database: 'workouterr-db'
+      }),
+      UsersModule],
   controllers: [],
   providers: [],
 })
