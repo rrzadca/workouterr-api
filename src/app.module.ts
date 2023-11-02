b152d2444c4abccc4ea5b081bb0674a55841b9e8
rrzadca <rrzadca@gmail.com>
2023-11-02 07:00:39 +0100

    format files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,20 @@
 import { Module } from '@nestjs/common';
-import {UsersModule} from "./crud/users/users.module";
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { UsersModule } from './crud/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-      TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'root',
-        password: 'example',
-        database: 'workouterr-db'
-      }),
-      UsersModule],
-  controllers: [],
-  providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: '127.0.0.1',
+            port: 3306,
+            username: 'root',
+            password: 'example',
+            database: 'workouterr-db',
+        }),
+        UsersModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
-import * as fs from "fs";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-      .setTitle('WorkouteRR API')
-      .setDescription('WorkouteRR REST API')
-      .setVersion('1.0')
-      .build();
-  const document = SwaggerModule.createDocument(app, config);
+    const config = new DocumentBuilder()
+        .setTitle('WorkouteRR API')
+        .setDescription('WorkouteRR REST API')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
 
-  fs.writeFileSync("./swagger-spec.json", JSON.stringify(document));
+    fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
 
-  SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+    await app.listen(3000);
 }
 bootstrap();
