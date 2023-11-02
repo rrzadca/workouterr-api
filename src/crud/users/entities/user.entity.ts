20bededbc98c1fb9180fb9a1038a03a59e37bfe7
rrzadca <rrzadca@gmail.com>
2023-11-02 06:59:44 +0100

    enable prettier, remove default eslint

--- src/crud/users/entities/user.entity.ts
+++ src/crud/users/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        unique: true,
+    })
+    username: string;
+
+    @Column()
+    createOn: Date;
+}
