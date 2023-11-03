09c0fb0c366d77d60c25fdae175fc9f9df099d19
rrzadca <rrzadca@gmail.com>
2023-11-03 07:44:53 +0100

    enable cors

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,7 +12,8 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
+import { from, map, Observable } from 'rxjs';
+import { UserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -21,36 +22,66 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Post()
-    create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    create(@Body() createUserDto: CreateUserDto): Observable<UserDto> {
         this.logger.log(`call create`);
-        return this.usersService.create(createUserDto);
+        return from(this.usersService.create(createUserDto)).pipe(
+            map(({ id, email, createdOn, updatedOn }) => ({
+                id,
+                email,
+                createdOn,
+                updatedOn,
+            })),
+        );
     }
 
     @Get()
-    findAll(): Promise<User[]> {
+    findAll(): Observable<UserDto[]> {
         this.logger.log(`call findAll`);
-        return this.usersService.findAll();
+        return from(this.usersService.findAll()).pipe(
+            map((users) =>
+                users.map(({ id, email, createdOn, updatedOn }) => ({
+                    id,
+                    email,
+                    createdOn,
+                    updatedOn,
+                })),
+            ),
+        );
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string): Promise<User> {
+    findOne(@Param('id') id: string): Observable<UserDto> {
         this.logger.log(`call findOne :id=${id}`);
-        return this.usersService.findOne(id);
+        return from(this.usersService.findOne(id)).pipe(
+            map(({ id, email, createdOn, updatedOn }) => ({
+                id,
+                email,
+                createdOn,
+                updatedOn,
+            })),
+        );
     }
 
     @Patch(':id')
     update(
         @Param('id') id: string,
         @Body() updateUserDto: UpdateUserDto,
-    ): Promise<User> {
+    ): Observable<UserDto> {
         this.logger.log(`call update :id=${id}`);
-        return this.usersService.update(id, updateUserDto);
+        return from(this.usersService.update(id, updateUserDto)).pipe(
+            map(({ id, email, createdOn, updatedOn }) => ({
+                id,
+                email,
+                createdOn,
+                updatedOn,
+            })),
+        );
     }
 
     @Delete(':id')
     @HttpCode(204)
-    remove(@Param('id') id: string): Promise<void> {
+    remove(@Param('id') id: string): Observable<void> {
         this.logger.log(`call remove :id=${id}`);
-        return this.usersService.remove(id);
+        return from(this.usersService.remove(id));
     }
 }
