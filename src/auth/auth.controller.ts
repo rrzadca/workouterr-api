2175600ffbc4ada71f7aab690a97130da8228c83
rrzadca <rrzadca@gmail.com>
2023-11-04 08:44:14 +0100

    generate JWT tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
     @Post('login')
     @UseGuards(AuthGuard('local'))
     async login(@Request() request: Request) {
-        return { userId: (request as any).user.id, token: 'Auth token' };
+        return {
+            userId: (request as any).user.id,
+            token: this.authService.getTokenForUser((request as any).user),
+        };
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
-    providers: [LocalStrategy],
+    providers: [LocalStrategy, AuthService],
     controllers: [AuthController],
-    imports: [UsersModule],
+    imports: [
+        UsersModule,
+        JwtModule.registerAsync({
+            useFactory: () => ({
+                secret: process.env.AUTH_SECRET,
+                signOptions: {
+                    expiresIn: '60m',
+                },
+            }),
+        }),
+    ],
 })
 export class AuthModule {}
